8ba3e444a8592f44c09b7789d6b170cd3c97717f
isha <ishavekariya913@gmail.com>
2024-03-05 14:57:34 +0530

    isha 4:51 pm  27-02-2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
-}
+
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-analytics")
+
+   }
